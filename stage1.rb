38fe1af7f5ab5f46ce915931f8144892e7b4d47f
Yuito Murase <yuito.murase@gmail.com>
2020-02-11 10:43:41 +0900

    Update test and fixing implementation

--- stage1.rb
+++ stage1.rb
@@ -1,23 +1,24 @@
 require 'yaml'
+require 'erb'
 
-OPAM_TEMPLATE = <<EOS
+OPAM_TEMPLATE = ERB.new(<<EOS
 opam-version: "2.0"
-name: "satysfi-fonts-computer-modern"
-version: "0.7.0+satysfi0.0.4"
-synopsis: "SATySFi Font Package for Donald Knuth's Computer Modern Fonts"
+name: "satysfi-<%= yml["name"] %>"
+version: "<%= yml["font-metadata"]["version"] %>+satysfi0.0.4"
+synopsis: "SATySFi Font Package for <%= yml["font-metadata"]["name"] %> fonts"
 description: """
-SATySFi font package for Donald Knuth's Computer Modern Fonts.
+SATySFi font package for <%= yml["font-metadata"]["name"] %> fonts.
 
-This package installs fonts from https://www.checkmyworking.com/cm-web-fonts/.
+This package installs fonts from <%= yml["font-metadata"]["url"] %>.
 """
-maintainer: "MURASE Yuito <yuito.murase@gmail.com>"
-authors: "MURASE Yuito <yuito.murase@gmail.com>"
-license: "OFL"
-homepage: "https://github.com/zeptometer/SATySFi-fonts-computer-modern"
-bug-reports: "https://github.com/zeptometer/SATySFi-fonts-computer-modern/issues"
-dev-repo: "git+https://github.com/zeptometer/SATySFi-fonts-computer-modern.git"
-extra-source "computer-modern.zip" {
-  archive: "https://www.checkmyworking.com/cm-web-fonts/Computer%20Modern.zip"
+maintainer: "<%= yml["author"]["name"] %> <<%= yml["author"]["email"] %>>"
+authors: "<%= yml["author"]["name"] %> <<%= yml["author"]["email"] %>>"
+license: "<%= yml["font-metadata"]["license"] %>"
+homepage: "<%= yml["site"]["homepage"] %>"
+bug-reports: "<%= yml["site"]["bug-reports"] %>"
+dev-repo: "<%= yml["site"]["dev-repo"] %>"
+extra-source "<%= yml["font-archive"]["name"] %>" {
+  archive: "<%= yml["font-archive"]["url"] %>"
   checksum: [
     "sha256=c240fff666a7d66de83a9f09ef045732c07291f2c83a07ae25ed888dc92f00f0"
     "sha512=fddad701425d1d1e761f1998b9a168fb60dff5972a858e44ae107e9bfa1678f807e71055b93b0969d29a0d728233a5532edc02c0655bded3ac5bc69b580da0c4"
@@ -28,7 +29,7 @@ depends: [
   "satyrographos" {>= "0.0.2" & < "0.0.3"}
 ]
 build: [
-  ["unzip" "-j" "-d" "computer-modern" "-o" "computer-modern.zip" "*.ttf"]
+  [<%= extract_command %>]
 ]
 install: [
   ["satyrographos" "opam" "install"
@@ -37,7 +38,17 @@ install: [
    "-script" "%{build}%/Satyristes"]
 ]
 EOS
+)
 
-def gen_opam(spec)
-    OPAM_TEMPLATE
+def gen_opam(yml)
+    extract_command = yml["font-archive"]["extract"].split(/\s/).map do |cmd|
+      "\"#{cmd}\""
+    end
+    .join(" ")
+
+    File.open("actual-#{yml["name"]}.txt", "w") do |f|
+      f.puts(OPAM_TEMPLATE.result(binding))
+    end
+
+    OPAM_TEMPLATE.result(binding)
 end
--- test/stage1-test.rb
+++ test/stage1-test.rb
@@ -6,13 +6,13 @@ class TestStage1 < Test::Unit::TestCase
 
     def test_opam_generated_for_computer_modern
         # given
-        yml = YAML.load_file('computer-modern-stage1.yml')
+        yml = YAML.load_file('computer-modern-unicode-stage1.yml')
 
         # when
         actual = gen_opam(yml)
 
         # then
-        expected = File.read('expected-computer-modern.opam')
+        expected = File.read('expected-computer-modern-unicode.opam')
         assert_equal(expected, actual)
     end
 
