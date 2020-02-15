2ea66abbed8bcd4bceb75aa825e8c7e0de60256f
Yuito Murase <yuito.murase@gmail.com>
2020-02-15 13:06:11 +0900

    Fix cjk sample text for doc

--- gen_doc_saty.rb
+++ gen_doc_saty.rb
@@ -12,7 +12,7 @@ DOC_SATY_TEMPLATE = <<~TEMPLATE
   @require: base/inline
 
   let lorem = {Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.}
-  let lemon = {何故だかその頃私は見すぼらしくて美しいものに強くひきつけられたのを覚えている。風景にしても壊れかかった街だとか、その街にしてもよそよそしい表通りよりもどこか親しみのある、汚い洗濯物が干してあったりがらくたが転がしてあったりむさくるしい部屋が覗のぞいていたりする裏通りが好きであった。}
+  let lemon = {何故だかその頃私は見すぼらしくて美しいものに強くひきつけられたのを覚えている。風景にしても壊れかかった街だとか、その街にしてもよそよそしい表通りよりもどこか親しみのある、汚い洗濯物が干してあったりがらくたが転がしてあったりむさくるしい部屋が覗いていたりする裏通りが好きであった。}
   let mathexample = {${\\abs{\\mathrm{Orb}_G\\(x\\)} \\cdot \\abs{\\mathrm{Stab}_G\\(x\\)} = \\abs{G}}}
 
   let make-heading fontname ctx =
